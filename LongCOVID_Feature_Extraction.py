2cdfa9d8fd45fa6e10c9bc8ea28dea25d9a96a87
emmy <emmythama@gmail.com>
2023-02-06 11:20:50 -0800

    updated version of feature extraction

--- LongCOVID_Feature_Extraction.py
+++ LongCOVID_Feature_Extraction.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 import os
 from datetime import *
+import math
 
 
-# In[19]:
+# In[2]:
 
 
 rootdir = "/labs/mpsnyder/long-covid-study-data/final_data"
 idmapping = pd.read_csv("/home/emmyst/idmapping.csv", dtype='str')
 date_format = "%Y-%m-%d"
+ID_bottom = []
 
 
-# In[20]:
+# In[3]:
 
-#Length of datapoints
+
 def count_len_csv(files, path):
     total_count = 0
     for f in files:
@@ -30,11 +32,10 @@ def count_len_csv(files, path):
     return total_count
 
 
-# In[21]:
+# In[17]:
 
-#Length of device usage time of first file not empty
+
 def device_length(files, path):
-    diff = []
     for f in files:
         updated = path + '/' + f
         df = pd.read_csv(updated)
@@ -42,13 +43,17 @@ def device_length(files, path):
         if not date.empty:
             start_date = date[0]
             end_date = date[len(df) -1]
-            formatted_start = datetime.strptime(start_date, date_format)
-            formatted_end = datetime.strptime(end_date, date_format)
-            return abs(formatted_start - formatted_end)
+            try:
+                formatted_start = datetime.strptime(start_date, date_format)
+                formatted_end = datetime.strptime(end_date, date_format)
+                return abs(formatted_start - formatted_end)
+            except:
+                print("Cannot convert date")
 
-# In[22]:
 
-#Number of gaps in data for all metrics per participant
+# In[18]:
+
+
 def gaps_numb(files, path):
     count = 0
     for f in files:
@@ -58,25 +63,30 @@ def gaps_numb(files, path):
         if not date_col.empty:
             prev = datetime.strptime(date_col[0], date_format)
             for i in range(1, len(df)):
-                cur_date = datetime.strptime(date_col[i], date_format)
-                if cur_date - prev > timedelta(days = 3):
-                    count += 1
-                prev = cur_date
+                cur_value = date_col[i]
+                try:
+                    cur_date = datetime.strptime(date_col[i], date_format)
+                    if cur_date - prev > timedelta(days = 3):
+                        count += 1
+                    prev = cur_date
+                except:
+                    print("cannot convert date")
+                    ID_bottom.append((os.path.basename(path), f))
     return count
 
 
-# In[23]:
+# In[19]:
 
-#Average values of different features (hr, st)
+
 def mean_features(path):
     df = pd.read_csv(path)
     if not df.empty:
         return df['Value'].mean()
 
 
-# In[ ]:
+# In[7]:
 
-#Pulling all together - finding for each participant in list
+
 dir_list = os.listdir(rootdir)
 data_count = []
 gaps_count = []
@@ -86,6 +96,7 @@ mean_st = []
 for dir in dir_list:
     path = rootdir + "/" + dir
     csv_files = os.listdir(path)
+    
     if len(csv_files) != 0:
         data_count.append(count_len_csv(csv_files, path))
         gaps_count.append(gaps_numb(csv_files, path))
@@ -94,10 +105,21 @@ for dir in dir_list:
             mean_hr.append(mean_features(path + "/" + "hr.csv"))
         if "st.csv" in csv_files:
             mean_st.append(mean_features(path + "/" + "st.csv"))
-    print("done")
+    print(dir)
 
 
-# In[17]:
+# In[8]:
+
+
+for dir in dir_list:
+    path = rootdir + "/" + dir
+    csv_files = os.listdir(path)
+    print(dir, csv_files)
+    if len(csv_files) != 0:
+        gaps_count.append(gaps_numb(csv_files, path))
+
+
+# In[ ]:
 
 
 #Saving to csv file
