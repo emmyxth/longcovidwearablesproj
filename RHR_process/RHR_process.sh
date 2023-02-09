a46c343f7d0918b172e14c5b4f1d32cadf232989
ely808chen <122660437+ely808chen@users.noreply.github.com>
2023-02-09 15:32:10 -0800

    Update RHR_process.sh
    changed the file path of output

--- RHR_process/RHR_process.sh
+++ RHR_process/RHR_process.sh
@@ -8,8 +8,8 @@
 #SBATCH --account=default
 #SBATCH --time=1:00:00
 #SBATCH --array=0-992
-#SBATCH --output=/labs/mpsnyder/long-covid-study-data/output/RHR_process/%A_%a.out
-#SBATCH --error=/labs/mpsnyder/long-covid-study-data/output/RHR_process/%A_%a.err
+#SBATCH --output=/labs/mpsnyder/LongCovidEkanath/output/RHR_process/%A_%a.out
+#SBATCH --error=/labs/mpsnyder/LongCovidEkanath/output/RHR_process/%A_%a.err
 
 module load miniconda/3
 FILES=(/labs/mpsnyder/LongCovidEkanath/COVID_Positives/COVID_Positives_Final_data/*)
